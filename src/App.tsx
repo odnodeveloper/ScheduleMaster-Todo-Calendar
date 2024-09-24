c52f1aba18b729c1240c0d8f6fdf9de307cc9412
Odonchimeg Myagmar <112740790+odnodeveloper@users.noreply.github.com>
2024-09-24 17:05:44 -0500

    Fixed layout error

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import LoginSignup from './components/LoginSignupPage/LoginSignup';
 
 function App() {
   return (
-    <main className="py-20 h-screen bg-blue-50 flex justify-center items-center text-center">
-      <div className="bg-white p-10 rounded-lg shadow-lg max-w-lg w-full flex flex-col justify-center items-center">
+    <main className="py-20 h-screen bg-blue-50 flex justify-center text-center items-center">
+      <div className="bg-white p-20 rounded-lg shadow-lg max-w-lg w-full flex flex-col justify-center">
         <div className="pb-10">
           <h1 className="font-bold text-3xl text-blue-600">SCHEDULE MASTER</h1>
-          <h2 className="font-bold text-2xl">TODO CALENDAR</h2>
+          <h2 className="font-bold text-2xl text-gray-600">TODO CALENDAR</h2>
         </div>
         <div>
           <LoginSignup />
